8453427a5b9aa054879b26bbb313a42e5a217656
mjtanfelizapc <micharltanfeliz6@gmail.com>
2025-02-12 19:30:55 +0800

    component

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import FoodItem from './components/FoodItem.vue'
+const app = createApp(App)
+app.component('food-item', FoodItem)
+app.mount('#app')
