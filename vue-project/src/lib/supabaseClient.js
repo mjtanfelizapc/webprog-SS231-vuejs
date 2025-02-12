a96266427d7e745e1101dce2baf46b8d30ccb5e2
mjtanfelizapc <micharltanfeliz6@gmail.com>
2025-02-12 23:28:30 +0800

    update

--- vue-project/src/lib/supabaseClient.js
+++ vue-project/src/lib/supabaseClient.js
@@ -1,4 +1,4 @@
-import { createclient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient('https://qhenfanfzgdieapttibm.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoZW5mYW5memdkaWVhcHR0aWJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkzNjgwODQsImV4cCI6MjA1NDk0NDA4NH0.2AYWfjt9qmJiogfw9QW37gzOXyz8mWEyOqCVi8cFfl0')
 
