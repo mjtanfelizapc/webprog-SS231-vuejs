9b5dd4024731c4b5516f60b8046e4154e598950a
mjtanfelizapc <micharltanfeliz6@gmail.com>
2025-02-12 22:29:19 +0800

    Create supabase client

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,6 +1,25 @@
-<script></script>
+ <script setup>
+  import { ref, onMounted } from 'vue'
+  import { supabase } from './lib/supabaseClient'
+
+  const instruments = ref([])
+
+  async function getInstruments() {
+    const { data } = await supabase.from('instruments').select()
+    instruments.value = data
+  }
+
+  onMounted(() => {
+    getInstruments()
+  })
+  </script>
 
 <template>
+
+<ul>
+      <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+    </ul>
+    
   <h1>Food</h1> 
   <personal-profile/> <br>
   <food-item/> <br>
